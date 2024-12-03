01e9b1027b1c5114c7f6a1d32f99cf7970f78873
YedixonRDev <yedixonjrf@gmail.com>
2024-12-03 13:44:58 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import './index.css';
+
+function App() {
+
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className='text-center'>FX KILLERS TRADING HOUSE</h1>
+      <div className="text-red-600">Texto de Prueba</div>
+
+    </div>
+  )
+}
+
+export default App
